c4cf7e6dca5f3af970d03fdfe62cba1ab282f7ee
sairivera <svrivera@live.com>
2024-06-28 21:47:42 +0800

    Uploaded the Settings enum class.

---
+++ Settings.cs
@@ -0,0 +1,32 @@
+﻿namespace Zapper.Enumerations
+{
+    /// <summary>
+    /// Enumerates the settings available for the user.
+    /// </summary>
+    public enum Settings : short
+    {
+        // SMS Notifications enabled
+        SMSNotifications = 1,
+
+        // Push Notifications enabled
+        PushNotifications,
+
+        // Bio-metrics enabled
+        BioMetrics,
+
+        // Camera enabled
+        Camera,
+
+        // Location enabled
+        Location,
+
+        // NFC enabled
+        NFC,
+
+        // Vouchers enabled
+        Vouchers,
+
+        // Loyalty enabled
+        Loyalty
+    }
+}
